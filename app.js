502ce82c4ddbd949c16958ac237f3ceb5d358060
asanthosh200210@gmail.com <asanthosh200210@gmail.com>
2024-02-05 10:21:37 +0530

    added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express=require('express');
 const https=require('https');
 const app=express();
+require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html")
@@ -28,7 +29,7 @@ app.post("/signup",function(req,res){
     //https request using this option object what kind of request in this case post and also for accesing we need key that is the api key
     const options={
         method:"POST",
-        auth:"santhosh:15c2b3debb61eeac37e4aa74f4b2ea7c-us21"
+        auth:process.env.API_KEY
     }
     const url="https://us21.api.mailchimp.com/3.0/lists/843903887f";
     const request=https.request(url,options,function(response){
@@ -55,7 +56,7 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 })
 //this for heroku server
-app.listen(process.env.PORT||3000,function(){
+app.listen(process.env.PORT,function(){
     console.log("sever is runing on port 3000");
 });
 
